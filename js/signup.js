471057ff918cc07734e0cf12050af9e63782171d
Anamika993 <anamika.varshney_cs20@gla.ac.in>
2022-07-24 16:51:13 +0530

    Initial commit

---
+++ js/signup.js
@@ -0,0 +1,19 @@
+// alert("helo")
+const signupBtn = document.querySelector(".signupBtn");
+const loginBtn = document.querySelector(".loginBtn");
+const moveBtn = document.querySelector(".moveBtn");
+const signup = document.querySelector(".signup");
+const login = document.querySelector(".Login");
+
+loginBtn.addEventListener("click",()=>{
+    moveBtn.classList.add("rightBtn");
+    moveBtn.innerHTML = "Login";
+    login.classList.add("loginform");
+    signup.classList.remove("signupform");
+})
+signupBtn.addEventListener("click",()=>{
+    moveBtn.classList.remove("rightBtn");
+    login.classList.remove("loginform");
+    signup.classList.add("signupform");
+    moveBtn.innerHTML = "Signup";
+})
